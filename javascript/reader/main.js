efbe72006bf8783e853c2d721ac7afaf5ecc7189
Sk0ut <poke_flavio@hotmail.com>
2015-12-23 15:11:59 +0000

    Yet another lib update

--- javascript/reader/main.js
+++ javascript/reader/main.js
@@ -40,7 +40,6 @@ main=function()
 	myInterface.setScene(myScene);
 
     myInterface.setActiveCamera(myScene.camera);
-
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
 	
