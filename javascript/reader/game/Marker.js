96dea9201705a41fb6cf249517f3edb61e5dedf3
sceptross <afonso.castro11@gmail.com>
2015-12-27 17:59:05 +0000

    Commit

--- javascript/reader/game/Marker.js
+++ javascript/reader/game/Marker.js
@@ -11,16 +11,16 @@ Marker.prototype = Object.create(CGFobject.prototype);
 Marker.prototype.constructor = Marker;
 
 Marker.prototype.charToCoords = function(c){
-	if(/^[a-zA-Z0-9]{1}$/.test(c)){
-		console.error('The marker has unsupported characters');
+	if(!/^[a-zA-Z0-9]{1}$/.test(c)){
+		console.error(c + ' is not a supported character');
 		return;
 	}
-	if(!/^[0-9]{1}$/.test(c))
-		return [c.parseInt(),3];
+	if(/^[0-9]{1}$/.test(c))
+		return [c.charCodeAt() - '0',3];
 	var pos = c.charCodeAt() - 'a'.charCodeAt()+1; 
-	if(!/^[a-z]{1}$/.test(c))
+	if(/^[a-z]{1}$/.test(c))
 		return [6+(pos/16),pos%16];
-	if(!/^[A-Z]{1}$/.test(c))
+	if(/^[A-Z]{1}$/.test(c))
 		return [4+(pos/16),pos%16];
 }
 
@@ -28,8 +28,9 @@ Marker.prototype.display = function(){
 	var currShader = this.scene.activeShader;
 	this.scene.setActiveShaderSimple(this.shader);
 	this.scene.pushMatrix();
-	this.fontTexture.bind();
+		this.fontTexture.bind();
 		this.scene.scale(10,10,10);
+		this.scene.translate(0,this.string.length/2,0);
 		for(var c = 0; c < this.string.length; ++c){
 			var pos = this.charToCoords(this.string[c]);
 			if(pos == undefined)
@@ -38,7 +39,7 @@ Marker.prototype.display = function(){
 			this.plane.display();
 			this.scene.translate(1,0,0);
 		}
-	this.fontTexture.unbind();
+		this.fontTexture.unbind();
 	this.scene.popMatrix();
 	this.scene.setActiveShaderSimple(currShader);
 }
