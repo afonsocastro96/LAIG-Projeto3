3d5e01b8721e8460d341b0b444094c80074b32f1
sceptross <afonso.castro11@gmail.com>
2016-01-04 19:31:51 +0000

    Removing useless file

--- cenas.pl
+++
@@ -1,9 +0,0 @@
-extrai_pos_par(List, Result) :- extrai_pos_par_aux(List, Result, 0).
-extrai_pos_par_aux([], [], _).
-extrai_pos_par_aux([Elem|List], [Elem|Result], N) :- Rem is mod(N,2), !, Rem is 1, X is N+1, extrai_pos_par_aux(List, Result, X).
-extrai_pos_par_aux([_|List], Result, N) :- X is N+1, extrai_pos_par_aux(List, Result, X).
-
-extrai_geral(List, Result, Rule) :- extrai_geral_aux(List, Result, Rule).
-extrai_geral_aux([], [], _).
-extrai_geral_aux([Elem|List], [Elem|Result], Rule) :- Rule(Elem), extrai_geral_aux(List, Result, Rule).
-extrai_geral_aux([_|List], Result, Rule) :- extrai_geral_aux(List, Result, Rule).
